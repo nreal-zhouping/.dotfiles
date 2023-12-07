bfc802d2742fd291bc82db71a34b03adf63dac03
zhouping <nreal-zhouping>
2023-12-07 16:50:57 +0800

    feat(nvim):add install vim into setup.sh. support codeium

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+
+# 检查是否安装了 nvim
+if command -v nvim &> /dev/null; then
+    echo "nvim 已经安装."
+else
+    # 提示用户是否要安装 nvim
+    read -p "nvim 未安装，是否要安装？ (y/n): " answer
+
+    # 如果用户选择是，执行安装命令
+    if [ "$answer" == "y" ]; then
+        echo "开始安装nvim..."
+        sudo add-apt-repository ppa:neovim-ppa/unstable
+        sudo apt-get update
+        sudo apt-get install neovim
+    else
+        echo "取消安装."
+        exit 1
+    fi
+fi
+
+
 if [[ ! -d ~/.config ]]
 then
     mkdir ~/.config
@@ -14,13 +35,14 @@ fi
     mv ~/.config/nvim ~/.config/nvim.backup
 fi
 
-ln -sf ~/.dotfiles_huangcan/nvim ~/.config/nvim
+ln -sf ~/.dotfiles/nvim ~/.config/nvim
 
 if [[ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]]
 then
     echo "git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim"
     git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
+
 nvim +PackerSync
 echo "Please install language server"
 echo "sudo apt install nodejs npm && sudo npm install -g bash-language-server pyright vscode-langservers-extracted typescript typescript-language-server"
